b43ed1f662aa3f193e32b7d2069640ff21b385c5
Ogor_bd43@student.itstep.org <Ogor_bd43@student.itstep.org>
2023-10-04 23:55:05 +0300

    Add Settings Controller and View

---
+++ Forum_MVC/Controllers/SettingsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Forum_MVC.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult ProfileSettings()
+        {
+            return View();
+        }
+    }
+}
--- Forum_MVC/wwwroot/js/site.js
+++ Forum_MVC/wwwroot/js/site.js
@@ -1,3 +1,7 @@
 ﻿function redirectToLoginPage() {
     window.location.href = '/SignInSignUpForm/SignInForm'; 
 }
+
+function redirectToProfileSettings() {
+    window.location.href = '/Settings/ProfileSettings';
+}
